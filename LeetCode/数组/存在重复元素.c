fa7ed2881d48cb04998d8f921a2908ade192400a
BlackChg <2279295980@qq.com>
2021-01-23 19:08:20 +0800

    存在重复元素.c

---
+++ LeetCode/数组/存在重复元素.c
@@ -0,0 +1,20 @@
+int CmpInt(const void* a, const void* b)
+{
+    return *(int*)a - *(int*)b;
+}
+
+bool containsDuplicate(int* nums, int numsSize)
+{
+    if (numsSize == 0 || numsSize == 1)
+        return false;
+    
+    qsort(nums, numsSize, sizeof(int), CmpInt);
+
+    for (int i = 0; i < numsSize - 1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+            return true;
+    }
+
+    return false;
+}
